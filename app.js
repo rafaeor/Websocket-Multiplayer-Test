919610f6425e362f531806cca369eacd73a9563e
Rafaeor <astomeri@protonmail.com>
2023-03-20 22:19:58 -0300

    Concertar express routing

---
+++ app.js
@@ -0,0 +1,75 @@
+import { Server } from 'socket.io';
+import express from 'express';
+import { createServer } from 'http';
+
+const app = express(); 
+const server = createServer(app); 
+const socketio = new Server(server);
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", function(req, res) {
+
+  res.sendFile(__dirname + "/client/index.html");
+
+});
+
+app.use("/client", express.static(__dirname + "/client"));
+
+
+
+server.listen(2000);
+
+console.log("Server started.");
+
+
+
+const SOCKET_LIST = {};
+/*static onConnect(socket) {
+
+    let player = new Player(socket.id);
+
+    socket.on("keyPress", function(data) {
+
+      if (data.inputId === "left") player.pressingLeft = data.state;
+
+      else if (data.inputId === "right") player.pressingRight = data.state;
+
+      else if (data.inputId === "up") player.pressingUp = data.state;
+
+      else if (data.inputId === "down") player.pressingDown = data.state;
+
+    });
+
+  }
+
+
+
+  static onDisconnect(socket) {
+
+    delete Player.list[socket.id];
+
+  }
+
+
+  setInterval(function() {
+
+    let pack = {
+  
+      player: Player.update()
+  
+    };
+  
+    for (let i in SOCKET_LIST) {
+  
+      let socket = SOCKET_LIST[i];
+  
+      socket.emit("newPosition", pack);
+  
+    }
+  
+  }, 1000 / 25);*/
